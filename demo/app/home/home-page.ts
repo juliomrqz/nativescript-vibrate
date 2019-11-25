41d0e26ae454d48adff1589858655afdd5a4a51c
Julio Marquez <juliocmrqzm@gmail.com>
2019-11-24 23:00:42 -0300

    chore(demo): update demo

---
+++ demo/app/home/home-page.ts
@@ -0,0 +1,16 @@
+/*
+In NativeScript, a file with the same name as an XML file is known as
+a code-behind file. The code-behind is a great place to place your view
+logic, and to set up your page’s data binding.
+*/
+
+import { Vibrate } from 'nativescript-vibrate';
+import { NavigatedData, Page } from "tns-core-modules/ui/page";
+
+import { HomeViewModel } from "./home-view-model";
+
+export function onNavigatingTo(args: NavigatedData) {
+    const page = <Page>args.object;
+
+    page.bindingContext = new HomeViewModel();
+}
---
+++ demo/app/home/home-view-model.ts
@@ -0,0 +1,39 @@
+import { Vibrate } from 'nativescript-vibrate';
+import { Observable } from "tns-core-modules/data/observable";
+
+export class HomeViewModel extends Observable {
+    public hasVibrator: string;
+    public vibrator: Vibrate;
+
+    constructor() {
+        super();
+        this.vibrator = new Vibrate();
+
+        // Has vibrator
+        this.hasVibrator = 'Has vibrator?: ' + this.vibrator.hasVibrator();
+    }
+
+    // Basic vibration
+    basicVibrate(eventData) {
+        console.log('Basic vibrate');
+        this.vibrator.vibrate();
+    }
+
+    // One seconds vibration
+    oneSecondVibrate(eventData) {
+        this.vibrator.vibrate(1000);
+        console.log('1 seconds vibrate');
+    }
+
+    // Pattern vibration
+    patternVibrate(eventData) {
+        this.vibrator.vibrate([1000, 300, 500, 2000]);
+        console.log('Pattern vibrate');
+    }
+
+    // Cancel vibration
+    cancelVibrate(eventData) {
+        this.vibrator.cancel();
+        console.log('Cancel vibrate');
+    }
+}
